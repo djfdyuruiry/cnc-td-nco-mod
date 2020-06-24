e0f270519dfa7b54c5f72417865e72bc5104c107
djfdyuruiry <djfdyuruiry@noreply.github.com>
2020-06-24 19:17:18 +0100

    MSBuild in workflow

--- scripts/build.bat
+++ scripts/build.bat
@@ -6,7 +6,7 @@ set config=%~1
 
 call "%~dp0lib\log.bat" "INFO" "Building bin\Win32\TiberianDawn.dll - %config%"
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" ^
+MSBuild.exe ^
   /p:configuration=%config% ^
   /t:clean ^
   /t:build ^
