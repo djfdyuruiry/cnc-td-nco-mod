4f446ad684f7aa1b669b175bcea56951b6c688e9
djfdyuruiry <djfdyuruiry@noreply.github.com>
2020-06-26 22:54:32 +0100

    Moving to a static Lua library

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -18,13 +18,8 @@ mkdir "%MOD_PATH%\Data"
 
 copy "game\ccmod.json" "%MOD_PATH%\ccmod.json"
 copy "bin\Win32\TiberianDawn.dll" "%MOD_PATH%\Data"
-copy "lib\lua53.dll" "%MOD_PATH%\Data"
 
 call "%~dp0lib\log.bat" "INFO" "Deploying Lua 5.3, RULES.INI and tiberian_dawn.bat to path: %CNC_PATH%"
-
-if exist "%CNC_PATH%\lua53.dll" (
-  del "%CNC_PATH%\lua53.dll"
-)
 
 if exist "%CNC_PATH%\RULES.INI" (
   del "%CNC_PATH%\RULES.INI"
@@ -38,7 +33,6 @@ if exist "%CNC_PATH%\tiberian_dawn.bat" (
   del "%CNC_PATH%\tiberian_dawn.bat"
 )
 
-copy "lib\lua53.dll" "%CNC_PATH%"
 copy "game\lua\test.lua" "%CNC_PATH%"
 copy "game\cfg\RULES.INI" "%CNC_PATH%"
 copy "game\scripts\tiberian_dawn.bat" "%CNC_PATH%"
