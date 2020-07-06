a09720b4655bb458789e9984cb1f08ed1cba35e1
djfdyuruiry <djfdyuruiry@noreply.github.com>
2020-07-06 20:17:40 +0100

    Fixing test dll path

--- scripts/run-tests.bat
+++ scripts/run-tests.bat
@@ -4,7 +4,7 @@ call "%~dp0lib\log.bat" "INFO" "Running tests"
 
 pushd "%~dp0..\tests\Win32\Debug"
 
-vstest.console "%~dp0..\tests\Win32\Debug\Nco.Tests.dll"
+vstest.console "Nco.Tests.dll"
 
 popd
 
