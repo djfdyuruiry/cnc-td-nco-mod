478d59ea96b31fcaf7fbc32f9e28c5b64ca59d1c
djfdyuruiry <djfdyuruiry@noreply.github.com>
2020-07-10 22:02:38 +0100

    Fixing incorrect game rule and adding more rule get/set validation tests

--- game/lua/test-lua-rules.lua
+++ game/lua/test-lua-rules.lua
@@ -1,5 +1,4 @@
-function main()
-  local typeAreas =
+local typeAreas =
   {
     infantry = {
       getTypes = getInfantryTypes,
@@ -31,23 +30,31 @@ function main()
     }
   }
 
+function main()
   local oldLogLevel = getLogLevel()
   setLogLevel("info");
 
-  log("Testing Lua API get/set rules")
+  log(">Testing Lua API get/set rules")
 
   local areasProcessed = 0
   local typesProcessed = 0
   local rulesRead = 0
   local rulesWritten = 0
+  local gameRulesRead = 0
+  local gameRulesWritten = 0
+  local validationErrors = 0
+  local gameRuleValidationErrors = 0
+
+  local validationFailed = false
 
   local status, err = pcall(function()
     for area, typeArea in pairs(typeAreas) do
+      log(string.format(">>Testing %s get/set rules", area))
 
       for _, areaType in ipairs(typeArea.getTypes()) do
+        log(string.format(">>>Testing type %s get/set rules", areaType))
 
         for _, ruleName in ipairs(typeArea.getRuleNames()) do
-
           local ruleValue = typeArea.getRuleValue(areaType, ruleName)
 
           rulesRead = rulesRead + 1
@@ -55,6 +62,13 @@ function main()
           typeArea.setRuleValue(areaType, ruleName, ruleValue)
 
           rulesWritten = rulesWritten + 1
+
+          if ruleValue ~= typeArea.getRuleValue(areaType, ruleName) then
+            showError(string.format("Validation of type rule value failed %s -> %s", areaType, ruleName))
+
+            validationFailed = true
+            validationErrors = validationErrors + 1
+          end
 
           ::next::
         end
@@ -64,7 +78,30 @@ function main()
       
       areasProcessed = areasProcessed + 1
     end
+    
+    log(string.format(">>Testing get/set game rules", typeArea))
+
+    for _, gameRule in ipairs(getGameRuleNames()) do
+        log(string.format("Game rule: %s", gameRule))
+
+        local ruleValue = getGameRule(gameRule)
+
+        gameRulesRead = gameRulesRead + 1
+
+        setGameRule(gameRule, ruleValue)
+        
+        gameRulesWritten = gameRulesWritten + 1
+
+        if ruleValue ~= getGameRule(gameRule) then
+          showError(string.format("Validation of game rule value failed %s", gameRule))
+
+          validationFailed = true
+          gameRuleValidationErrors = gameRuleValidationErrors + 1
+        end
+    end
   end)
+
+  status  = status and not validationFailed
 
   setLogLevel(oldLogLevel);
 
@@ -76,12 +113,20 @@ function main()
   Types processed: %d
   Rules read: %d
   Rules written: %d
+
+  Game rules read: %d
+  Game rules written: %d
+
+  Validation errors: %d
 ]],
       status and "PASS" or "FAIL",
       areasProcessed,
       typesProcessed,
       rulesRead,
-      rulesWritten
+      rulesWritten,
+      gameRulesRead,
+      gameRulesWritten,
+      validationErrors
     )
   )
 
