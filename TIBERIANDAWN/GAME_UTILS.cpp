e471fc5acc04f3537b03502d2b531b7ec1ddec8a
djfdyuruiry <djfdyuruiry@noreply.github.com>
2020-07-16 23:25:17 +0100

    Adding refresh sidebar implementation

---
+++ TIBERIANDAWN/GAME_UTILS.cpp
@@ -0,0 +1,22 @@
+#include "function.h"
+
+#include "DLLInterface.h"
+
+void Show_Game_Notification(char* message, float durationInSeconds)
+{
+	On_Message(message, durationInSeconds);
+}
+
+void Refresh_Sidebar()
+{
+	for (auto index = 0; index < Buildings.Count(); index++) {
+		Buildings.Ptr(index)->Update_Buildables();
+	}
+
+	Refresh_Game_Map();
+}
+
+void Refresh_Game_Map()
+{
+	Map.Flag_To_Redraw(true);
+}
