6e454256bb74089d2b09ff963abcf3c99a5840d0
PG-SteveT <63470275+PG-SteveT@users.noreply.github.com>
2020-08-12 11:32:36 -0700

    August 12th Hotfix
    Includes fix for infinite loop in FootClass::Detach

--- TIBERIANDAWN/DLLInterface.cpp
+++ TIBERIANDAWN/DLLInterface.cpp
@@ -297,6 +297,7 @@ class DLLExportClass {
 		static int CurrentDrawCount;
 		static int TotalObjectCount;
 		static int SortOrder;
+		static int ExportLayer;
 		static CNCObjectListStruct *ObjectList;
 
 
@@ -336,6 +337,7 @@ class DLLExportClass {
 int DLLExportClass::CurrentDrawCount = 0;
 int DLLExportClass::TotalObjectCount = 0;
 int DLLExportClass::SortOrder = 0;
+int DLLExportClass::ExportLayer = 0;
 CNCObjectListStruct *DLLExportClass::ObjectList = NULL;
 SidebarGlyphxClass DLLExportClass::MultiplayerSidebars [MAX_PLAYERS];
 uint64 DLLExportClass::GlyphxPlayerIDs[MAX_PLAYERS] = {0xffffffffl};
@@ -2995,7 +2997,11 @@ void DLLExportClass::DLL_Draw_Intercept(int shape_number, int x, int y, int widt
 
 	new_object.CNCInternalObjectPointer = (void*)object;
 	new_object.OccupyListLength = 0;
-	new_object.SortOrder = SortOrder++;
+	if (CurrentDrawCount == 0) {
+		new_object.SortOrder = (ExportLayer << 29) + (object->Sort_Y() >> 3);
+	} else {
+		new_object.SortOrder = ObjectList->Objects[TotalObjectCount].SortOrder + CurrentDrawCount;
+	}
 
 	strncpy(new_object.TypeName, object->Class_Of().IniName, CNC_OBJECT_ASSET_NAME_LENGTH);
 
@@ -3363,6 +3369,8 @@ bool DLLExportClass::Get_Layer_State(uint64 player_id, unsigned char *buffer_in,
 	*/
 	for (int layer = 0; layer < DLL_LAYER_COUNT; layer++) {
 		
+		ExportLayer = layer;
+
 		for (int index = 0; index < Map.Layer[layer].Count(); index++) {
 			
 			ObjectClass *object = Map.Layer[layer][index];
