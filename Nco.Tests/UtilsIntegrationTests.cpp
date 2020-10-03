1a8f13ed36e2c469ef431effc6956eea5891239d
djfdyuruiry <djfdyuruiry@noreply.github.com>
2020-10-03 19:49:35 +0100

    Refactoring tests

--- Nco.Tests/UtilsIntegrationTests.cpp
+++ Nco.Tests/UtilsIntegrationTests.cpp
@@ -11,159 +11,165 @@ static auto TEST_FILENAME = "test_file.txt";
 
 namespace Integration
 {
-	TEST_CLASS(UtilsIntegrationTests)
+	namespace Nco
 	{
-		HANDLE fileHandle;
-
-		public:
-			TEST_METHOD_INITIALIZE(Setup)
+		namespace Common
+		{
+			TEST_CLASS(UtilsIntegrationTests)
 			{
-				if (PathFileExists(TEST_FILENAME))
+				HANDLE fileHandle;
+
+			public:
+				TEST_METHOD_INITIALIZE(Setup)
 				{
-					DeleteFile(TEST_FILENAME);
+					if (PathFileExists(TEST_FILENAME))
+					{
+						DeleteFile(TEST_FILENAME);
+					}
 				}
-			}
 
-			TEST_METHOD_CLEANUP(TearDown)
-			{
-				if (fileHandle != NULL && fileHandle != INVALID_HANDLE_VALUE)
+				TEST_METHOD_CLEANUP(TearDown)
 				{
+					if (fileHandle != NULL && fileHandle != INVALID_HANDLE_VALUE)
+					{
+						CloseHandle(fileHandle);
+						fileHandle = NULL;
+					}
+				}
+
+				TEST_METHOD(When_Get_Env_Var_IsCalled_WithPresentVarName_ThenValueIsReturned)
+				{
+					Assert::AreEqual(
+						"Windows_NT",
+						Get_Env_Var("OS", NULL)
+					);
+				}
+
+				TEST_METHOD(When_Get_Env_Var_IsCalled_WithPresentVarName_ThenValueFoundIsSetToTrue)
+				{
+					bool valueFound = false;
+
+					Get_Env_Var("OS", &valueFound);
+
+					Assert::IsTrue(valueFound);
+				}
+
+				TEST_METHOD(When_Get_Env_Var_Or_WithMissingVarName_ThenValueFoundIsSetToFalse)
+				{
+					bool valueFound = true;
+
+					Get_Env_Var("CARGO_SIZE", &valueFound);
+
+					Assert::IsFalse(valueFound);
+				}
+
+				TEST_METHOD(When_Open_File_For_Appending_IsCalled_WithValidNewFileName_ThenNoErrorIsReturned)
+				{
+					bool errorOccurred = false;
+
+					fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
+
+					Assert::AreEqual(
+						false,
+						errorOccurred
+					);
+				}
+
+				TEST_METHOD(When_Open_File_For_Appending_IsCalled_WithValidNewFileName_ThenHandleIsReturned)
+				{
+					bool errorOccurred = false;
+
+					fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
+
+					Assert::AreNotEqual(
+						INVALID_HANDLE_VALUE,
+						fileHandle
+					);
+				}
+
+				TEST_METHOD(When_Open_File_For_Appending_IsCalled_WithValidExistingFileName_ThenNoErrorIsReturned)
+				{
+					bool errorOccurred = false;
+
+					fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
 					CloseHandle(fileHandle);
-					fileHandle = NULL;
+
+					fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
+
+					Assert::AreEqual(
+						false,
+						errorOccurred
+					);
 				}
-			}
 
-			TEST_METHOD(When_Get_Env_Var_IsCalled_WithPresentVarName_ThenValueIsReturned)
-			{
-				Assert::AreEqual(
-					"Windows_NT",
-					Get_Env_Var("OS", NULL)
-				);
-			}
+				TEST_METHOD(When_Open_File_For_Appending_IsCalled_WithValidExistingFileName_ThenHandleIsReturned)
+				{
+					bool errorOccurred = false;
 
-			TEST_METHOD(When_Get_Env_Var_IsCalled_WithPresentVarName_ThenValueFoundIsSetToTrue)
-			{
-				bool valueFound = false;
+					fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
 
-				Get_Env_Var("OS", &valueFound);
+					CloseHandle(fileHandle);
 
-				Assert::IsTrue(valueFound);
-			}
+					fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
 
-			TEST_METHOD(When_Get_Env_Var_Or_WithMissingVarName_ThenValueFoundIsSetToFalse)
-			{
-				bool valueFound = true;
+					Assert::AreNotEqual(
+						INVALID_HANDLE_VALUE,
+						fileHandle
+					);
+				}
 
-				Get_Env_Var("CARGO_SIZE", &valueFound);
+				TEST_METHOD(When_Open_File_For_Appending_IsCalled_WithInvalidFileName_ThenErrorIsReturned)
+				{
+					bool errorOccurred = false;
 
-				Assert::IsFalse(valueFound);
-			}
+					fileHandle = Open_File_For_Appending("Y:\\somewhere\\test_file.txt", &errorOccurred);
 
-			TEST_METHOD(When_Open_File_For_Appending_IsCalled_WithValidNewFileName_ThenNoErrorIsReturned)
-			{
-				bool errorOccurred = false;
+					Assert::AreEqual(
+						true,
+						errorOccurred
+					);
+				}
 
-				fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
+				TEST_METHOD(When_Append_To_File_IsCalled_WithValidHandle_ThenStringIsWrittenToFile)
+				{
+					char* expected = "Hello there!";
 
-				Assert::AreEqual(
-					false,
-					errorOccurred
-				);
-			}
+					// append
+					bool errorOccurred = false;
 
-			TEST_METHOD(When_Open_File_For_Appending_IsCalled_WithValidNewFileName_ThenHandleIsReturned)
-			{
-				bool errorOccurred = false;
+					fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
 
-				fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
+					Append_To_File(fileHandle, expected);
 
-				Assert::AreNotEqual(
-					INVALID_HANDLE_VALUE,
-					fileHandle
-				);
-			}
+					CloseHandle(fileHandle);
 
-			TEST_METHOD(When_Open_File_For_Appending_IsCalled_WithValidExistingFileName_ThenNoErrorIsReturned)
-			{
-				bool errorOccurred = false;
+					// read
+					auto readBuffer = Allocate_String(strlen(expected));
 
-				fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
-				CloseHandle(fileHandle);
+					fileHandle = CreateFile(
+						TEST_FILENAME,
+						GENERIC_READ,
+						FILE_SHARE_READ,
+						NULL,
+						OPEN_EXISTING,
+						FILE_ATTRIBUTE_NORMAL,
+						NULL
+					);
 
-				fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
+					ReadFile(
+						fileHandle,
+						readBuffer,
+						13,
+						NULL,
+						NULL
+					);
 
-				Assert::AreEqual(
-					false,
-					errorOccurred
-				);
-			}
-
-			TEST_METHOD(When_Open_File_For_Appending_IsCalled_WithValidExistingFileName_ThenHandleIsReturned)
-			{
-				bool errorOccurred = false;
-
-				fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
-
-				CloseHandle(fileHandle);
-
-				fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
-
-				Assert::AreNotEqual(
-					INVALID_HANDLE_VALUE,
-					fileHandle
-				);
-			}
-
-			TEST_METHOD(When_Open_File_For_Appending_IsCalled_WithInvalidFileName_ThenErrorIsReturned)
-			{
-				bool errorOccurred = false;
-
-				fileHandle = Open_File_For_Appending("Y:\\somewhere\\test_file.txt", &errorOccurred);
-
-				Assert::AreEqual(
-					true,
-					errorOccurred
-				);
-			}
-
-			TEST_METHOD(When_Append_To_File_IsCalled_WithValidHandle_ThenStringIsWrittenToFile)
-			{
-				char* expected = "Hello there!";
-
-				// append
-				bool errorOccurred = false;
-
-				fileHandle = Open_File_For_Appending(TEST_FILENAME, &errorOccurred);
-
-				Append_To_File(fileHandle, expected);
-
-				CloseHandle(fileHandle);
-
-				// read
-				auto readBuffer = Allocate_String(strlen(expected));
-
-				fileHandle = CreateFile(
-					TEST_FILENAME,
-					GENERIC_READ,
-					FILE_SHARE_READ,
-					NULL,
-					OPEN_EXISTING,
-					FILE_ATTRIBUTE_NORMAL,
-					NULL
-				);
-	
-				ReadFile(
-					fileHandle,
-					readBuffer,
-					13,
-					NULL,
-					NULL
-				);
-
-				Assert::AreEqual(
-					"Hello there!",
-					readBuffer
-				);
-			}
-	};
+					Assert::AreEqual(
+						"Hello there!",
+						readBuffer
+					);
+				}
+			};
+		}
+	}
 }
